96ba1ca577e1b0a45f608797f4955378553c5f7a
dhankins345 <11397228+dhankins345@users.noreply.github.com>
2023-04-11 23:19:43 -0500

    Make the data viewer work P1

--- data_viewer.py
+++ data_viewer.py
@@ -1,15 +1,17 @@
 import settings, csv
 
 stockDay = 1
-stockNumber = len(settings.stock_price)+1
 
 def textOutput(stockDay):
-    startLine = stockNumber * stockDay
+    marketLine = (3 * stockDay) - 3
+    marketStat = marketinfo[marketLine]
+    marketList = marketprice[marketLine]
+    marketChg = marketchange[marketLine]
     # Generate Header
     print("")
     print("SIMPLE STOCK MARKET SIMULATOR")
     print("---------------------------------------------------------------------------------")
-    print(f"Day: {stockDay}   +++   Total Market at Close: ${closeTotal}   +++   Change in Market: {changeMark}")
+    print(f"Day: {stockDay}   +++   Total Market at Close: ${marketStat[1]}   +++   Change in Market: {marketStat[2]}")
     print("---------------------------------------------------------------------------------")
 
     # Enumerate Stocks
@@ -29,11 +31,25 @@ def textOutput(stockDay):
     if wait == "Q" or wait == "q":
         quit()
 
+# create lists to store values from csv
+marketinfo = []
+marketprice = []
+marketchange = []
+dayRows = 1
+dayCSV = 1
+
 # load values from CSV
-lineAmount = stockDay * len(settings.stock_price)
 with open(settings.datafile, 'r') as inputfile:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = csv.reader(inputfile, delimiter=',')
     for row in csv_reader:
-        
+        if dayRows == 1:
+            marketinfo.append(row)
+        if dayRows == 2:
+            marketprice.append(row)
+        if dayRows == 3:
+            marketchange.append(row)
+            dayCSV += 1
+            dayRows = 0
+        dayRows += 1
 
 textOutput(stockDay)
